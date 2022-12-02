24924a4e207bf0e4fa3e25f625af12d74c601e0a
SteinChr <c.steinberger@gibs.at>
2022-12-02 15:11:02 +0100

    2022 Day 02 Added

---
+++ 2022/02/part2.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from 'fs';
+console.time('Time');
+let input = readFileSync('./input.txt', 'utf-8').split('\r\n').map(x => x.split(/\s/));
+let result = 0;
+
+for (let i = 0; i < input.length; i++) {
+    if (input[i][0] == 'A') {
+        if (input[i][1] == 'X') {
+            result += 3;
+        } else if (input[i][1] == 'Y') {
+            result += 4;
+        } else if (input[i][1] == 'Z') {
+            result += 8;
+        }
+    } else if (input[i][0] == 'B') {
+        if (input[i][1] == 'X') {
+            result += 1;
+        } else if (input[i][1] == 'Y') {
+            result += 5;
+        } else if (input[i][1] == 'Z') {
+            result += 9;
+        }
+    } else if (input[i][0] == 'C') {
+        if (input[i][1] == 'X') {
+            result += 2;
+        } else if (input[i][1] == 'Y') {
+            result += 6;
+        } else if (input[i][1] == 'Z') {
+            result += 7;
+        }
+    }
+}
+
+console.log(result);
+console.timeEnd('Time');
