6430036e42605a7bb91e1b7f1ce038d0f5cca9da
SteinChr <c.steinberger@gibs.at>
2022-12-23 16:30:52 +0100

    2022 Day 13 Added

---
+++ 2022/13/part1.ts
@@ -0,0 +1,45 @@
+import { readFileSync } from 'fs';
+console.time('Time');
+let input = readFileSync('./input.txt', 'utf-8').split('\r\n\r\n').map(x => x.split('\r\n'));
+let result = 0;
+
+for (let i = 0; i < input.length; i++) {
+    let left = eval(input[i][0]);
+    let right = eval(input[i][1]);
+    if (checkPair(left, right) == 'Yes') {
+        result += i + 1;
+    }
+}
+
+console.log(result);
+
+function checkPair(left, right): string {
+    let status = 'Processing';
+    if (typeof(left) == 'number' && typeof(right) == 'number') {
+        status = left < right ? 'Yes' : left > right ? 'No' : 'Processing';
+    } else if (Array.isArray(left) && Array.isArray(right) && left.length == 0 && right.length == 0) {
+        status = 'Processing';
+    } else if (Array.isArray(left) && left.length == 0) {
+        status = 'Yes';
+    } else if (Array.isArray(right) && right.length == 0) {
+        status = 'No';
+    } else {
+        let leftArray = left;
+        let rightArray = right;
+
+        if (typeof(left) == 'number') leftArray = [left];
+        if (typeof(right) == 'number') rightArray = [right];
+
+        for (let i = 0; i < leftArray.length && status == 'Processing'; i++) {
+            if (i > rightArray.length - 1) {
+                status = 'No';
+            } else {
+                status = checkPair(leftArray[i], rightArray[i]);
+            }
+        }
+        if (status == 'Processing' && leftArray.length < rightArray.length) status = 'Yes';
+    }
+    return status;
+}
+
+console.timeEnd('Time');
