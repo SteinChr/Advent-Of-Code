2f6c682596d82dbb698410da92d9ba020e9e8322
SteinChr <c.steinberger@gibs.at>
2026-09-12 16:13:29 +0200

    2017 Day 06 Added

---
+++ 2017/06/part2.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+console.time('Time');
+let input = readFileSync('./input.txt', 'utf-8').split(/\t|\s/).map(x => parseInt(x));
+let result = 0;
+
+let seen = new Map<string, number>();
+
+for (let i = 1; true; i++) {
+    let blocks = Math.max(...input);
+    let index = input.indexOf(blocks);
+
+    input[index] = 0;
+    index++;
+
+    while (blocks > 0) {
+        if (index >= input.length) index = 0;
+
+        input[index]++;
+        index++;
+        blocks--;
+    }
+
+    let inputString = input.join('_');
+
+    if (seen.has(inputString)) {
+        result = i - seen.get(inputString);
+        break;
+    }
+
+    seen.set(inputString, i);
+}
+
+console.log(result);
+console.timeEnd('Time');
